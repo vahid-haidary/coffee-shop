ca1e52d5d1bc918e729be1bc26da424597bff3a6
vahid haidary <vahidhaidary0077@gmail.com>
2023-11-28 21:18:40 +0330

    menu-res-else-submenu

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,13 @@
-const toggleThemeBtn = document.querySelector("#toggle-theme");
-toggleThemeBtn.addEventListener("click" , () => {
-    if (localStorage.theme === "dark"){
-        document.documentElement.classList.remove("dark");
-        localStorage.theme = "light";
-    } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme" , "dark");
-    }
+const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+
+toggleThemeBtns.forEach(btn => {
+    btn.addEventListener("click" , function () {
+        if (localStorage.theme === "dark"){
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme" , "dark");
+        } 
+    })
 })
